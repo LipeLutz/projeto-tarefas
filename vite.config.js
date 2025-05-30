806e1b5fef3aefbceaf8481edf5e64827a2735b1
Felipe Lutz <felipeclutz@gmail.com>
2025-05-29 23:37:44 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/projeto-tarefas"
+  base: process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true'
+    ? '/projeto-tarefas/'
+    : '/',
 })
